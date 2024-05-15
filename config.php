9164997f3d674fdaa95b3d3e820cf24bc831debb
Joel <111261582+joelwsy@users.noreply.github.com>
2024-05-15 20:30:21 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = 'RM';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'db_testing1';
-$db_host = 'localhost';
+$db_username = 'admin';
+$db_password = 'Cloud123';
+$db_name = 'tarumtpos';
+$db_host = 'tarumtrds.cwdhhxz6rmvz.us-east-1.rds.amazonaws.com';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
