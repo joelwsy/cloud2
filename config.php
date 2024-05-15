ec12f59360a2dfeb8fbdf921a54b0fb7fe17047c
Joelw <joelwsy-pm22@student.tarc.edu.my>
2024-05-15 10:52:34 +0800

    Initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$currency = 'RM';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'db_testing1';
+$db_host = 'localhost';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
---
+++ db_testing1.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2024 at 06:27 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_testing1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(15) NOT NULL,
+  `product_code` varchar(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_desc` varchar(255) NOT NULL,
+  `price` int(10) NOT NULL,
+  `units` int(5) NOT NULL,
+  `total` int(15) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `email` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `product_code`, `product_name`, `product_desc`, `price`, `units`, `total`, `date`, `email`) VALUES
+(14, 'TAR3', 'Yarn and Bear Plushie Bundle', 'Bouquet made from yarn, along with plushie bears', 100, 1, 100, '2024-05-14 15:45:21', 'joelwsy-pm22@student.tarc.edu.my');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_code` varchar(60) NOT NULL,
+  `product_name` varchar(60) NOT NULL,
+  `product_desc` tinytext NOT NULL,
+  `product_img_name` varchar(60) NOT NULL,
+  `qty` int(5) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `qty`, `price`) VALUES
+(1, 'TAR1', 'Calypso Sunflower & Rose Bouquet', 'Bouquet', '1.png', 26, 179.00),
+(2, 'TAR2', 'Mika Graduation Bear Bouquet', 'Graduation Bear plushies', '2.png', 9, 139.00),
+(3, 'TAR3', 'Yarn and Bear Plushie Bundle', 'Bouquet made from yarn, along with plushie bears', '3.png', 29, 100.00),
+(4, 'TAR4', 'Mika Graduation Bear Plush', 'Mika bear plush', '4.png', 120, 50.00),
+(5, 'TAR5', 'Personalised Necklace', 'Customise your own necklace', '5.png', 39, 39.00),
+(6, 'TAR6', 'Personalised Keychains', 'Customise your own keychains', '6.png', 1253, 19.00),
+(7, 'TAR7', 'Plaque', 'Customise your own plaque', '7.png', 123, 29.00),
+(8, 'TAR8', 'TinkerBell Sunflower', 'Mini bouquet', '8.png', 12, 122.00),
+(9, 'TAR9', 'Fairywell Sunflower', 'Sunflower bouquet', '9.png', 20, 120.00),
+(10, 'TAR10', 'Bouquets Cheyenne', 'Cheyennes bouquet set', '10.png', 11, 119.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `pin` int(6) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `type` varchar(20) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `address`, `city`, `pin`, `email`, `password`, `type`) VALUES
+(1, 'Steve', 'Jobs', 'Infinite Loop', 'California', 95014, 'sjobs@apple.com', 'steve', 'admin'),
+(3, 'Joel', 'W', '16, Tingkat ', 'Georgetown', 123123, 'joelwsy-pm22@student.tarc.edu.my', '123123', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_code` (`product_code`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
